07e2c54667ad150ba843f3f93a67b708a1bf79fc
sudipta khanra <sudiptakhanra0612@gmail.com>
2025-09-17 11:46:30 +0530

    Deployment Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,2 +1,8 @@
-const server = "http://localhost:8000"; 
+// const server = "http://localhost:8000";
+// export default server;
+
+let IS_PROD = true;
+const server = IS_PROD
+  ? "https://meetmaster-backend.onrender.com"
+  : "http://localhost:8000";
 export default server;
