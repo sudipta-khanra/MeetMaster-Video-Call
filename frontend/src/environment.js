3fbe0b8af2ac6a570ae3472cb710e3379be25955
Sudipta Khanra <sudiptakhanra0612@gmail.com>
2026-06-07 13:25:59 +0530

    Fix error

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 // const server = "http://localhost:8000";
 // export default server;
 
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD
   ? 'https://meetmaster-backend.onrender.com'
   : 'http://localhost:8000';
